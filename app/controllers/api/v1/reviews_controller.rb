23253cd2b8c16e284b63e81ba68900700d797423
Brennan <bmkk85@mst.edu>
2021-09-08 15:24:18 -0500

    Added routes and controllers for APIs

---
+++ app/controllers/api/v1/reviews_controller.rb
@@ -0,0 +1,48 @@
+
+module Api
+    module V1
+      class ReviewsController < ApplicationController
+        protect_from_forgery with: :null_session
+  
+        # POST /api/v1/reviews
+        def create
+          review = Review.new(review_params)
+  
+          if review.save
+            render json: serializer(review)
+          else
+            render json: errors(review), status: 422
+          end
+        end
+  
+        # DELETE /api/v1/reviews/:id
+        def destroy
+          review = Review.find(params[:id])
+  
+          if review.destroy
+            head :no_content
+          else
+            render json: errors(review), status: 422
+          end
+        end
+  
+        private
+  
+        # Strong params
+        def review_params
+          params.require(:review).permit(:title, :description, :score, :airline_id)
+        end
+  
+        # fast_jsonapi serializer
+        def serializer(review, options = {})
+          ReviewSerializer
+            .new(review)
+            .serializable_hash
+        end
+  
+        def errors(record)
+          { errors: record.errors.messages }
+        end
+      end
+    end
+  end
